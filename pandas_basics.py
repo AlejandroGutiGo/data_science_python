0ff524cd0ba2252b639a5faec86a684abfc92b9c
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-06-18 20:34:52 +0530

    Update pandas_basics.py

--- pandas_basics.py
+++ pandas_basics.py
@@ -6,7 +6,7 @@ headers = ["symboling","normalized-losses","make","fuel-type","aspiration", "num
          "drive-wheels","engine-location","wheel-base", "length","width","height","curb-weight","engine-type",
          "num-of-cylinders", "engine-size","fuel-system","bore","stroke","compression-ratio","horsepower",
          "peak-rpm","city-mpg","highway-mpg","price"]
-df.columns=headers
+df.columns=headers 
 df.dtypes
 df.info
 df[['price','bore']].describe(include= "all")
