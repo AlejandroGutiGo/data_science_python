024afe770f6d827dd7a002a6991e8c43ed68a30f
Ashu Kumar <30663362+kin-kins@users.noreply.github.com>
2018-08-27 23:35:33 +0530

    Update binning_data.py

--- binning_data.py
+++ binning_data.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np 
+import numpy as np  
 path="https://archive.ics.uci.edu/ml/machine-learning-databases/autos/imports-85.data"
 df = pd.read_csv(path,header=None)
 print("Done reading the csv files")
